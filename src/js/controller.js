94d6496475fcacff08be2e4efd2ce7edc52b7db0
vendyggod <charli98@mail.ru>
2024-03-16 20:39:50 +0300

    fetching data function

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,28 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const getRecipe = async function () {
+  try {
+    // Fetching data from recipes API
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message);
+
+    // Changing to camelCase format
+    const recipe = Object.keys(data.data.recipe).reduce((acc, key) => {
+      const formattedKey = key.replace(/_([a-z])/g, (_, letter) =>
+        letter.toUpperCase()
+      );
+      acc[formattedKey] = data.data.recipe[key];
+      return acc;
+    }, {});
+
+    console.log(recipe);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getRecipe();
